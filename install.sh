762d355c2555263feb24587c3fd4dc7328a9529c
Khandaker Ashikul Hoque <kh.ashik96@gmail.com>
2026-05-25 14:50:50 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-sudo apt install "${SCRIPT_DIR}/ftia-gui_1.0.0_amd64.deb"
+sudo apt install "${SCRIPT_DIR}/ftia-gui_3.0.1_amd64.deb"
